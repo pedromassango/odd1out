f32279bc6c28e6cfe0aa7616d2bebf21906799a5
pedromassango <pedromassango.developer@gmail.com>
2019-02-16 00:23:03 +0100

    initial commit

---
+++ lib/challenge.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Challenge{
+  final String odd;
+  final String common;
+  final Color color;
+
+  Challenge(this.odd, this.common, {this.color});
+}
---
+++ lib/games.dart
@@ -0,0 +1,14 @@
+
+import 'package:odd1out/challenge.dart';
+
+class Games{
+
+  final List<Challenge> games = [
+    Challenge('0', 'O'),
+    Challenge('B', 'D'),
+    Challenge('I', 'L'),
+    Challenge('B', '8'),
+    Challenge('N', 'M'),
+  ];
+
+}
