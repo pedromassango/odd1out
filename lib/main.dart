21004b1f73475271c0100fb62f701c2c1d439b95
pedromassango <pedromassango.developer@gmail.com>
2019-02-16 01:36:24 +0100

    new game on click done

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,114 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:odd1out/challenge.dart';
+import 'package:odd1out/games.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Odd1Out!!',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  MyHomePage({Key key}) : super(key: key);
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  int totalElements = 100;
+  bool isGameRunning;
+  Challenge challenge;
+  var random = Random();
+
+  generateGame(){
+    print('generateGame');
+    generated = false;
+    isGameRunning = true;
+    challenge = Games().getRandomChallenge();
+  }
+
+
+  bool generated = false;
+  int lastOddPosition;
+  String getGameItem(int index) {
+    var r = random.nextInt(totalElements);
+    var result = challenge.common;
+
+    print('First IF> $index & $r');
+
+    if (index == r && generated == false) {
+      if(lastOddPosition == r) {
+        return getGameItem(index);
+      }
+        result = challenge.odd;
+      generated = true;
+      lastOddPosition = r;
+    } else if(index == totalElements-1 && generated == false){
+      if(lastOddPosition == r) {
+        return getGameItem(index);
+      }
+      result = challenge.odd;
+      lastOddPosition = r;
+      generated = true;
+    }
+
+
+    return result;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIOverlays([]);
+
+    generateGame();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+   return Scaffold(
+     body: Padding(
+       padding: const EdgeInsets.fromLTRB(0, 32, 0, 16),
+       child: Column(
+         mainAxisSize: MainAxisSize.max,
+         crossAxisAlignment: CrossAxisAlignment.center,
+         children: <Widget>[
+           Text("Pick the Odd One Out!"),
+           Spacer(),
+           GridView.count(
+             crossAxisCount: 10,
+             shrinkWrap: true,
+             physics: NeverScrollableScrollPhysics(),
+             children: List.generate(totalElements, (i){
+               return Text(
+                 getGameItem(i),
+                 style: TextStyle(color: Colors.black,
+                   fontSize: 32
+                 ),
+               );
+             }),
+           ),
+           Spacer(),
+           GestureDetector(
+             onTap: (){
+               setState(() {
+                 generateGame();
+               });
+             },
+               child: Text('Skip Round', style: TextStyle(color: Colors.deepOrange),)),
+         ],
+       ),
+     ),
+   );
   }
 }
